ae69fdd31755194c6ea4d3a37ad2c9903dad8be3
Jamie Furness <reines@fluxbb.org>
2011-03-21 21:39:54 +0000

    Adding a MySQL dialect class.

---
+++ dialects/mysql.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * SQL Dialect for MySQL
+ * 
+ * Copyright (C) 2011 FluxBB (http://fluxbb.org)
+ * License: LGPL - GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.html)
+ */
+
+class SQLDialect_MySQL extends SQLDialect
+{
+
+}
