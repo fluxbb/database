1745351901d6ba41bd6f026c827fe27c26ab7160
Franz Liedke <franz@develophp.org>
2011-11-03 14:54:47 +0100

    Replace PHPUnit bootstrap file (XML) with a PHP bootstrap file.

---
+++ tests/bootstrap_example.php
@@ -0,0 +1,13 @@
+<?php
+
+$GLOBALS['DB_MYSQL_DBNAME'] = 'fluxbb__test';
+$GLOBALS['DB_MYSQL_HOST'] = 'localhost';
+$GLOBALS['DB_MYSQL_USER'] = 'root';
+$GLOBALS['DB_MYSQL_PASSWD'] = '';
+
+$GLOBALS['DB_SQLITE_FILE'] = ':memory:';
+
+$GLOBALS['DB_PGSQL_DBNAME'] = 'fluxbb__test';
+$GLOBALS['DB_PGSQL_HOST'] = 'localhost';
+$GLOBALS['DB_PGSQL_USER'] = 'root';
+$GLOBALS['DB_PGSQL_PASSWD'] = '';
