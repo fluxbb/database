4d236fa2c280a9be4120844b77711e5baaa46752
Franz Liedke <franz@develophp.org>
2011-11-11 00:24:14 +0100

    Move "default" implementation of tableInfo to MySQL adapter, since it is really driver-specific.
    Related to #534.

--- src/Database/Adapter.php
+++ src/Database/Adapter.php
@@ -788,63 +788,7 @@ abstract class Flux_Database_Adapter
 		return (bool) $this->query($sql)->fetchColumn();
 	}
 
-	public function runTableInfo(Flux_Database_Query_TableInfo $query)
-	{
-		$table = $query->getTable();
-		if (empty($table))
-			throw new Exception('A TABLE INFO query must have a table specified.');
-		
-		$table_info = array(
-			'columns'		=> array(),
-			'primary_key'	=> array(),
-			'unique'		=> array(),
-			'indices'		=> array(),
-		);
-
-		// Fetch column information
-		$result = $this->query('DESCRIBE '.$table);
-		foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $row)
-		{
-			$table_info['columns'][$row['Field']] = array(
-				'type'			=> $row['Type'],
-				'default'		=> $row['Default'],
-				'allow_null'	=> $row['Null'] == 'YES',
-			);
-		}
-
-		// Fetch all indices
-		$result = $this->query('SHOW INDEXES FROM '.$table);
-		foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $row)
-		{
-			// Save primary key
-			if ($row['Key_name'] == 'PRIMARY')
-			{
-				$table_info['primary_key'][] = $row['Column_name'];
-				continue;
-			}
-
-			if (!isset($table_info['indices'][$row['Key_name']]))
-			{
-				$table_info['indices'][$row['Key_name']] = array(
-					'fields'	=> array(),
-					'unique'	=> $row['Non_unique'] != 1,
-				);
-
-				if ($row['Non_unique'] != 1)
-				{
-					$table_info['unique'][] = array($row['Column_name']);
-				}
-			}
-			else
-			{
-				$table_info['unique'][count($table_info['unique']) - 1][] = $row['Column_name'];
-			}
-
-			$table_info['indices'][$row['Key_name']]['fields'][] = $row['Column_name'];
-		}
-
-		return $table_info;
-	}
+	abstract public function runTableInfo(Flux_Database_Query_TableInfo $query);
 
 	protected function compileColumnDefinition(Flux_Database_Query_Helper_TableColumn $column)
 	{
